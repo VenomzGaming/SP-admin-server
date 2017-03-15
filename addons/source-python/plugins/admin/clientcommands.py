b688e2286d8badad615f357727c8eaac182ffe74
Exiistenz <lilian78800@hotmail.fr>
2017-03-15 11:41:42 +0100

    clientcommands.py moved to commands folder

--- addons/source-python/plugins/admin/clientcommands.py
+++
@@ -1,8 +0,0 @@
-from .commands.manager import command_manager
-from .commands.filters import Filter
-from .permissions import check_permission
-from .menus import *
-
-@command_manager.add_command('sp_admin', 'a')
-def _show_admin_menu(player):
-	main_menu.send(player)
