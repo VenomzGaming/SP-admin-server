a97f76fc1b4ab8d896b54d06a0c71732992f9627
Exiistenz <lilian78800@hotmail.fr>
2017-03-15 14:18:46 +0100

    Modify mysql to use _Database

--- addons/source-python/plugins/admin/database/mysql.py
+++ addons/source-python/plugins/admin/database/mysql.py
@@ -2,30 +2,24 @@
 
 from pymysql import connect
 
-from ..config import MYSQL_ADDRESS
-from ..config import MYSQL_PORT
-from ..config import MYSQL_LOGIN
-from ..config import MYSQL_PASSWORD
-from ..config import MYSQL_DATABASE_NAME
-from .sqlite import SQLite
+# TODO add mysql configs
+#from ..config import MYSQL_ADDRESS
+#from ..config import MYSQL_PORT
+#from ..config import MYSQL_LOGIN
+#from ..config import MYSQL_PASSWORD
+#from ..config import MYSQL_DATABASE_NAME
+from .database import _Database
 
 ## ALL DECLARATION
 
 __all__ = (
     'MySQL',
-    )
+)
 
 ## 
 
-class MySQL(SQLite):
-    '''
-
-    MySQL database manager. Loads
-    data into <Player> instances, and
-    saves data from <Player> instances
-    to the database.
-
-    '''
+class MySQL(_Database):
+    ''' MySQL database manager.'''
 
     def __init__(self, host=MYSQL_ADDRESS, port=MYSQL_PORT, user=MYSQL_LOGIN, pw=MYSQL_PASSWORD, db=MYSQL_DATABASE_NAME):
         self.connection = connect(host=host, port=port, user=user, password=pw, db=db)
